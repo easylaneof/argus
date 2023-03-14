2f1a2ad53e770870f8fbbe069aca0e546485df4d
easylaneof <fokysland@yandex.ru>
2023-03-14 08:55:32 +0300

    refactor(parser): improve tests

--- link-parser/src/test/java/ru/tinkoff/edu/java/parser/LinkParserServiceTestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package ru.tinkoff.edu.java.parser;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("ru.tinkoff.edu.java.parser")
-class LinkParserServiceTestConfiguration {
-}
