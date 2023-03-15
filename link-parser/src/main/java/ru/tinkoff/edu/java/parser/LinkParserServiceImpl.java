6aae5f97e8f4d2545935abc3e0d29959c1aaae85
easylaneof <fokysland@yandex.ru>
2023-03-15 19:10:10 +0300

    refactor(link-parser): add default method for string url
    HW1

--- link-parser/src/main/java/ru/tinkoff/edu/java/parser/LinkParserServiceImpl.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/parser/LinkParserServiceImpl.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.tinkoff.edu.java.parser.uri.UriParser;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Objects;
 
@@ -16,17 +14,9 @@ public class LinkParserServiceImpl implements LinkParserService {
 
     @Override
     public ParsingResult parse(String url) {
-        URI uri;
-
-        try {
-            uri = new URI(url);
-        } catch (URISyntaxException e) {
-            return null;
-        }
-
         return parsers
                 .stream()
-                .map(parser -> parser.parse(uri))
+                .map(parser -> parser.parse(url))
                 .filter(Objects::nonNull)
                 .findFirst()
                 .orElse(null);
--- link-parser/src/main/java/ru/tinkoff/edu/java/parser/uri/UriParser.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/parser/uri/UriParser.java
@@ -4,7 +4,16 @@ import jakarta.annotation.Nullable;
 import ru.tinkoff.edu.java.parser.ParsingResult;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
 public interface UriParser {
+    default @Nullable ParsingResult parse(String url) {
+        try {
+            return parse(new URI(url));
+        } catch (URISyntaxException e) {
+            return null;
+        }
+    }
+
     @Nullable ParsingResult parse(URI uri);
 }
