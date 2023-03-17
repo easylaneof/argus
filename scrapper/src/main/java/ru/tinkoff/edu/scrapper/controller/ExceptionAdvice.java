aebdf391da132d73ab8657d07976b7fb06abd2fe
easylaneof <fokysland@yandex.ru>
2023-03-17 15:57:21 +0300

    feat(scrapper): added exception handling
    HW2

---
+++ scrapper/src/main/java/ru/tinkoff/edu/scrapper/controller/ExceptionAdvice.java
@@ -0,0 +1,33 @@
+package ru.tinkoff.edu.scrapper.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingRequestValueException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import ru.tinkoff.edu.scrapper.dto.ApiErrorResponse;
+
+import java.util.List;
+
+@ControllerAdvice
+public class ExceptionAdvice extends ResponseEntityExceptionHandler {
+    @ExceptionHandler({MissingRequestValueException.class})
+    public ResponseEntity<ApiErrorResponse> handleValidationException(MissingRequestValueException exception) {
+        ProblemDetail detail = exception.getBody();
+
+        return new ResponseEntity<>(
+                new ApiErrorResponse(
+                        detail.getDetail(),
+                        Integer.toString(detail.getStatus()),
+                        detail.getTitle(),
+                        exception.getMessage(),
+                        List.of() // TODO: not sure if we need to return stacktrace (maybe only in dev)
+                ),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    // TODO: add more exceptions + handlers
+}
