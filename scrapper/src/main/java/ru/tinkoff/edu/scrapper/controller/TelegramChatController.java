cd62b96969fe9eadc81efdd36fe2a069a6b99b04
easylaneof <fokysland@yandex.ru>
2023-03-17 16:38:44 +0300

    feat: added open-api spec generation
    HW2

--- scrapper/src/main/java/ru/tinkoff/edu/scrapper/controller/TelegramChatController.java
+++ scrapper/src/main/java/ru/tinkoff/edu/scrapper/controller/TelegramChatController.java
@@ -1,7 +1,5 @@
 package ru.tinkoff.edu.scrapper.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +14,7 @@ public class TelegramChatController {
     }
 
     @DeleteMapping("/tg-chat/{id}")
-    public ResponseEntity<LinkResponse> deleteChat(@PathVariable long id) {
-        if (id == -1) { // stub
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-        return new ResponseEntity<>(new LinkResponse(0, ""), HttpStatus.OK);
+    public LinkResponse deleteChat(@PathVariable long id) {
+        return new LinkResponse(0, "");
     }
 }
