b83e0eb3707a955ec834fab02af537514d329884
easylaneof <fokysland@yandex.ru>
2023-04-11 16:11:15 +0300

    feat(scrapper): call services in controllers
    HW5

--- scrapper/src/main/java/ru/tinkoff/edu/scrapper/controller/TelegramChatController.java
+++ scrapper/src/main/java/ru/tinkoff/edu/scrapper/controller/TelegramChatController.java
@@ -1,18 +1,28 @@
 package ru.tinkoff.edu.scrapper.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.tinkoff.edu.scrapper.dto.LinkResponse;
+import ru.tinkoff.edu.scrapper.service.ChatService;
 
 @RestController
+@RequiredArgsConstructor
+@Slf4j
 public class TelegramChatController {
+    private final ChatService chatService;
+
     @PostMapping("/tg-chat/{id}")
     public void registerChat(@PathVariable long id) {
+        log.info("Registering chat with id {}", id);
+        chatService.register(id);
     }
 
     @DeleteMapping("/tg-chat/{id}")
-    public void deleteChat(@PathVariable long id) {
+    public void unregisterChat(@PathVariable long id) {
+        log.info("Unregistering chat with id {}", id);
+        chatService.unregister(id);
     }
 }
