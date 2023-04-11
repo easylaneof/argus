534593158da9467ec119ddf99b1e29db8220ea0b
easylaneof <fokysland@yandex.ru>
2023-04-11 15:37:19 +0300

    feat(scrapper): added chat entity and repository
    HW5

---
+++ scrapper/src/main/java/ru/tinkoff/edu/scrapper/entity/Chat.java
@@ -0,0 +1,12 @@
+package ru.tinkoff.edu.scrapper.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Chat {
+    private Long id;
+}
