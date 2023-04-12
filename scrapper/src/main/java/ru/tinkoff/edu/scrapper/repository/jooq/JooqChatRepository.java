e79eb85f4c2e135b95c179e2ac1208a2131f856a
easylaneof <fokysland@yandex.ru>
2023-04-13 01:00:26 +0300

    feat(scrapper): added jooq chat repository
    HW6

---
+++ scrapper/src/main/java/ru/tinkoff/edu/scrapper/repository/jooq/JooqChatRepository.java
@@ -0,0 +1,57 @@
+package ru.tinkoff.edu.scrapper.repository.jooq;
+
+import lombok.RequiredArgsConstructor;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+import ru.tinkoff.edu.scrapper.entity.Chat;
+import ru.tinkoff.edu.scrapper.repository.ChatRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static ru.tinkoff.edu.scrapper.entity.jooq.Tables.CHAT;
+
+@RequiredArgsConstructor
+@Repository
+public class JooqChatRepository implements ChatRepository {
+    private final DSLContext create;
+
+    @Override
+    public void save(Chat entity) {
+        create
+                .insertInto(CHAT)
+                .values(entity.getId())
+                .execute();
+    }
+
+    @Override
+    public List<Chat> findAll() {
+        return create
+                .select(CHAT.ID)
+                .from(CHAT)
+                .fetchInto(Chat.class);
+    }
+
+    @Override
+    public Optional<Chat> findById(Long id) {
+        return create
+                .select(CHAT.ID)
+                .from(CHAT)
+                .where(CHAT.ID.eq(id))
+                .fetchOptionalInto(Chat.class);
+    }
+
+    @Override
+    public long count() {
+        return create
+                .fetchCount(CHAT);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        create
+                .delete(CHAT)
+                .where(CHAT.ID.eq(id))
+                .execute();
+    }
+}
