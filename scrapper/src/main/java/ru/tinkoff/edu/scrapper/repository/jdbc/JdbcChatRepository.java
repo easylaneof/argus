5a047f07b902ca1a67e654e240a816b29f285a89
easylaneof <fokysland@yandex.ru>
2023-04-16 00:22:46 +0300

    feat(scrapper): disable repositories auto-scan and remove @Primary
    HW7

--- scrapper/src/main/java/ru/tinkoff/edu/scrapper/repository/jdbc/JdbcChatRepository.java
+++ scrapper/src/main/java/ru/tinkoff/edu/scrapper/repository/jdbc/JdbcChatRepository.java
@@ -14,7 +14,6 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 @Repository
-@Primary // TODO: replace with configuration
 public class JdbcChatRepository implements ChatRepository {
     private static final String SAVE_SQL = """
             INSERT INTO chat(id)
