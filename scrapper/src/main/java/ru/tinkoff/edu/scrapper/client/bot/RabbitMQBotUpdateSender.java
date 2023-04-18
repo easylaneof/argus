a70a244beb98149c5a7f0196fb0591eff68582af
easylaneof <fokysland@yandex.ru>
2023-04-18 18:51:05 +0300

    feat(scrapper): added debug logs
    HW8

--- scrapper/src/main/java/ru/tinkoff/edu/scrapper/client/bot/RabbitMQBotUpdateSender.java
+++ scrapper/src/main/java/ru/tinkoff/edu/scrapper/client/bot/RabbitMQBotUpdateSender.java
@@ -1,15 +1,19 @@
 package ru.tinkoff.edu.scrapper.client.bot;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
 @RequiredArgsConstructor
+@Slf4j
 public class RabbitMQBotUpdateSender implements BotUpdateSender {
     private final RabbitTemplate rabbitTemplate;
     private final String topicExchangeName;
 
     @Override
     public void sendUpdates(LinkUpdateRequest request) {
+        log.info("Sending update to bot by rabbitmq {}", request);
+
         rabbitTemplate.convertAndSend(
                 topicExchangeName,
                 "links",
