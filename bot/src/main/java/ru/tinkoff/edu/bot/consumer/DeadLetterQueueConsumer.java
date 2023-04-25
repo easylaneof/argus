a991babe0e6a7c705d0e526c9a5b175a712123d0
easylaneof <fokysland@yandex.ru>
2023-04-25 12:36:38 +0300

    feat(bot): added dead letter queue
    HW8

---
+++ bot/src/main/java/ru/tinkoff/edu/bot/consumer/DeadLetterQueueConsumer.java
@@ -0,0 +1,17 @@
+package ru.tinkoff.edu.bot.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import ru.tinkoff.edu.bot.dto.LinkUpdate;
+
+@Slf4j
+@Component
+@RabbitListener(queues = "${app.scrapper-queue.dlq-name}")
+public class DeadLetterQueueConsumer {
+    @RabbitHandler
+    public void receiver(LinkUpdate update) {
+        log.info("Failed processing update: {}", update);
+    }
+}
