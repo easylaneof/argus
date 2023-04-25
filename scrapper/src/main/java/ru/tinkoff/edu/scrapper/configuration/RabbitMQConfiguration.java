939ae64ff3a0726843bb0b50ffb19ee2003dcd3e
easylaneof <fokysland@yandex.ru>
2023-04-25 12:37:25 +0300

    feat(scrapper): added dead letter queue
    HW8

--- scrapper/src/main/java/ru/tinkoff/edu/scrapper/configuration/RabbitMQConfiguration.java
+++ scrapper/src/main/java/ru/tinkoff/edu/scrapper/configuration/RabbitMQConfiguration.java
@@ -1,9 +1,9 @@
 package ru.tinkoff.edu.scrapper.configuration;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import jakarta.annotation.PostConstruct;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +13,10 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfiguration {
     @Bean
     public Queue queue(ApplicationProperties properties) {
-        return new Queue(properties.rabbitMq().queueName());
+        return QueueBuilder
+                .durable(properties.rabbitMq().queueName())
+                .withArgument("x-dead-letter-exchange", properties.rabbitMq().queueName() + ".dlx")
+                .build();
     }
 
     @Bean
@@ -29,5 +32,27 @@ public class RabbitMQConfiguration {
     @Bean
     public MessageConverter jsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    // FIXME?
+    // Rabbitmq-starter declares queues lazily.
+    // If there's no links yet, links queue won't be created.
+    // Bot begins listening for links queue on start
+    // But there's no links queue, so it fails to startup
+    // That's why we declare queue manually
+    @Bean
+    public RabbitAdmin rabbitAdmin(
+            ConnectionFactory connectionFactory,
+            TopicExchange exchange,
+            Queue queue,
+            Binding binding
+    ) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+
+        rabbitAdmin.declareExchange(exchange);
+        rabbitAdmin.declareQueue(queue);
+        rabbitAdmin.declareBinding(binding);
+
+        return rabbitAdmin;
     }
 }
